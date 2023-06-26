c6505fd5bf516a18315c7d346da3c8359eb69952
PABLOSleuth2 <opsnonman@gmail.com>
2023-06-26 17:27:57 +0600

    Update teleport at.lua

--- teleport at.lua
+++ teleport at.lua
@@ -2,6 +2,18 @@ local games = true
 print("Hello, World!")
 local pos = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
 local Players = game:GetService("Players")
+local plr = Players.buildNbc
+local char = plr.Character
+local human = char.Humanoid
+
+if human then
+  humanoid:Destroy()
+end
+
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(0, -498, 0)
-wait(0.1)
+char.Torso.Anchored = true
+wait(2)
+human = Instance.new("Humanoid")
+human.Parent = char
+char.Torso.Anchored = false
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(pos)
